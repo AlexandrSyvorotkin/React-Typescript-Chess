08db2e718b73c85d92f80a0ecf712a45fba35fa6
Alex <Alex031994@gmail.com>
2022-05-17 17:01:12 +0300

    implified board

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import BoardComponent from "./components/BoardComponent";
+import {Board} from "./models/Board";
 
 function App() {
+
+    const [board, setBoard] = useState(new Board())
+
+    useEffect(() => {
+        restart()
+    }, [])
+
+
+    function restart () {
+        const newBoard = new Board()
+        newBoard.initCells()
+        setBoard(newBoard)
+    }
+
     return (
-        <div className='w-screen h-screen flex justify-center items-center'>
-            work
+        <div className='w-screen h-screen flex justify-center items-center bg-slate-300'>
+            <BoardComponent board={board} setBoard={setBoard}/>
         </div>
     );
 }
---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,16 @@
+import React, {FC} from 'react';
+import {Cell} from "../models/Cell";
+
+interface cellProps {
+    cell: Cell;
+}
+
+const CellComponent: FC<cellProps> = ({cell}) => {
+    return (
+        <div className={['cell', cell.color].join(' ')}>
+
+        </div>
+    );
+};
+
+export default CellComponent;
