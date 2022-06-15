e052cdc08b135f32f5e9aa2f95c081cb0f39f7ff
Alex <Alex031994@gmail.com>
2022-06-15 17:34:07 +0300

    implemented game logic

--- src/components/BoardComponent.tsx
+++ src/components/BoardComponent.tsx
@@ -1,6 +1,7 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {Board} from "../models/Board";
 import CellComponent from "./CellComponent";
+import {Cell} from "../models/Cell";
 
 interface Boardprops {
     board: Board,
@@ -8,12 +9,44 @@ interface Boardprops {
 }
 
 const BoardComponent: FC<Boardprops> = ({board, setBoard}) => {
+
+    const [selectedCell, setSelectedCell] =  useState<Cell | null>(null)
+
+    function click(cell: Cell) {
+        if (selectedCell && selectedCell !== cell && selectedCell.figure?.canMove(cell)) {
+            selectedCell.moveFigure(cell)
+            setSelectedCell(null)
+        } else {
+            setSelectedCell(cell)
+        }
+
+    }
+
+    function hightlightCell() {
+        board.hightlightCells(selectedCell)
+        updateBoard()
+    }
+
+    useEffect(() => {
+        hightlightCell()
+    }, [selectedCell])
+
+    function updateBoard() {
+        const newBoard = board.getCopyBoard()
+        setBoard(newBoard)
+    }
+
     return (
         <div className='board'>
             {board.cells.map((row, index) =>
                 <React.Fragment >
                     {row.map(cell =>
-                        <CellComponent cell={cell} key={cell.id}/>
+                        <CellComponent
+                            cell={cell}
+                            key={cell.id}
+                            click={click}
+                            selected={cell.x === selectedCell?.x && cell.y === selectedCell?.y}
+                        />
                     )}
                 </React.Fragment>
             )}
